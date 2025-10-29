306a16ea396818e86f16e3c5a1e0fca2079c5475
shivam63717 <114639203+shivam63717@users.noreply.github.com>
2025-10-29 23:53:05 +0530

    Initial commit

--- components/data-explorer/data-profiler.tsx
+++ components/data-explorer/data-profiler.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState, useEffect } from 'react'
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Progress } from "@/components/ui/progress"
